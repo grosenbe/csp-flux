2fe20b88a383eda96900032ae73d89fbe824a87f
Geoff Rosenberg <geoff.rosenberg@gmail.com>
2023-03-07 21:03:26 -0800

    Switch to ubuntu for CI.

--- scripts/install_build_prereqs.sh
+++ scripts/install_build_prereqs.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-dnf install ninja-build clang gtest-devel
+sudo apt-get update
+sudo apt-get install ninja-build googletest valgrind clang
+export CC=$(which clang)
+export CXX=$(which clang++)
+
+# install googletest
+mkdir /usr/src/googletest/build
+cd /usr/src/googletest/build
+cmake -G "Ninja" ..
+ninja
+ninja install
+cd -
